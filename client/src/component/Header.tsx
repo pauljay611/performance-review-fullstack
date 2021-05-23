a3e6e14b3d756e27ba9601bbfe427a37afeed7ed
Paul Yang <paulyang@17.media>
2021-05-23 21:42:20 +0800

    fix logout

--- client/src/component/Header.tsx
+++ client/src/component/Header.tsx
@@ -43,7 +43,7 @@ const Header: React.FC<Props> = ({ paths }) => {
 
   const handleLogoutClick = () => {
     Cookies.remove("token");
-    history.push("/");
+    window.location.reload();
   };
 
   return (
