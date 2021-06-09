39fae37ad338cfd3bf445263d9a5f7c6505e8c91
Paul Yang <paulyang@17.media>
2021-06-09 11:34:10 +0800

    fix

--- server/src/auth/checkAdminRole.ts
+++ server/src/auth/checkAdminRole.ts
@@ -1,11 +1,11 @@
-export default (checkAdmin)=> (req, res, next) => {
-    try {
-        if(checkAdmin && !req.user.get('is_admin')){
-            return res.status(403).send('forbidden')
-        }
-        next()
-      } catch (error) {
-        error.statusCode = 400;
-        next(error);
-      }
-}
+export default (req, res, next) => {
+  try {
+    if (!req.user.get("is_admin")) {
+      return res.status(403).send("forbidden");
+    }
+    next();
+  } catch (error) {
+    error.statusCode = 400;
+    next(error);
+  }
+};
